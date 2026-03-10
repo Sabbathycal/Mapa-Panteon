a5996706492a86741d897e8e6521e17fe0e2b750
gguerra92 <77221428+gguerra92@users.noreply.github.com>
2026-03-10 19:39:17 +0000

    Rollback: regresar a estado de 2026-03-04 (ignorar cambios del 2026-03-10)

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 /* =========================================================
    CONFIG
    ========================================================= */
-const ASSET_VER = (window.APP_VERSION || Date.now());
-
-const BASE_IMAGE_PUBLIC_URL = "./assets/map/base-public.webp?v=" + ASSET_VER; // ligero (público)
-const BASE_IMAGE_EDIT_URL   = "./assets/map/base.png?v=" + ASSET_VER;         // alta res (edición)
+const BASE_IMAGE_PUBLIC_URL = "./assets/map/base-public.webp"; // ligero (público)
+const BASE_IMAGE_EDIT_URL   = "./assets/map/base.png";         // alta res (edición)
 
 // Tus GeoJSON están guardados en coordenadas del mapa base.png
 const DATA_COORD_WIDTH  = 11045;
