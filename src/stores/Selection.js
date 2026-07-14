26222b3d468c3ac240630ba640e866e12abf1d2d
Sabbathycal <182703131+Sabbathycal@users.noreply.github.com>
2026-07-14 14:53:56 -0600

    feat: Se agregaron las bases para Stores

---
+++ src/stores/Selection.js
@@ -0,0 +1,40 @@
+import {defineStore} from 'pinia'
+import {ref} from 'vue'
+
+export const useSelectionStore = defineStore('selection',() =>{
+    const selectedSectionId = ref(null)
+    const selectedBlockId = ref(null)
+    const selectedLotId = ref(null)
+
+    function selectSection(sectionId) {
+        selectedSectionId.value = sectionId
+        selectedBlockId.value = null
+        selectedLotId.value = null
+    }
+
+    function selectBlock(blockId) {
+        selectedBlockId.value = blockId
+        selectedLotId.value = null
+    }
+
+    function selectLot(lotId) {
+        selectedLotId.value = lotId
+    }
+
+    function clearSelection() {
+        selectedSectionId.value = null
+        selectedBlockId.value = null
+        selectedLotId.value = null
+    }
+
+    return {
+        selectedSectionId,
+        selectedBlockId,
+        selectedLotId,
+        selectSection,
+        selectBlock,
+        selectLot,
+        clearSelection
+    }
+
+})
