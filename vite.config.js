48f8fd4c6be060b8679c33d988fdfa3d0f5123e0
Sabbathycal <182703131+Sabbathycal@users.noreply.github.com>
2026-08-05 11:36:03 -0600

    Pepraracion para prueba githubpages

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,14 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+export default defineConfig(({ mode }) => ({
+  base: '/Mapa-Panteon/',
+
+  plugins: [vue(), mode === 'development' && vueDevTools()].filter(Boolean),
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
